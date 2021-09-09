e09743b15c027e861ab1e19f3e746a4bbf33091c
YizhePKU <yizhe@pku.edu.cn>
2021-09-09 17:57:14 +0800

    Fix test_canonicalize using same mr in assertions
    After `canonicalize` is rewritten, mr is updated in place but tests were
    not updated to reflect this.

--- test/test_conala.py
+++ test/test_conala.py
@@ -1,5 +1,6 @@
 import pytest
 import ast
+from copy import deepcopy
 
 from asdl.convert import ast_to_mr
 from conala import canonicalize, uncanonicalize
@@ -9,10 +10,11 @@ def test_canonicalize():
     intent = "Hello `variable`, 'string'."
     snippet = "print(variable, file='string')"
     mr = ast_to_mr(ast.parse(snippet))
+    original_mr = deepcopy(mr)
     new_intent, new_mr, ph2mr = canonicalize(intent, mr)
     assert new_intent == "Hello <ph_0>, <ph_1>."
     assert new_mr["body"][0]["value"]["args"][0]["id"] == "<ph_0>"
-    assert uncanonicalize(new_mr, ph2mr) == mr
+    assert uncanonicalize(new_mr, ph2mr) == original_mr
     assert len(ph2mr) == 2
 
 @pytest.mark.skip(reason="lists/set/dict not supported yet")
@@ -20,34 +22,38 @@ def test_canonicalize_double_backticks():
     intent = "`[1, 2]` and `[3, 4]`"
     snippet = "print([1, 2], [3, 4])"
     mr = ast_to_mr(ast.parse(snippet))
+    original_mr = deepcopy(mr)
     new_intent, new_mr, ph2mr = canonicalize(intent, mr)
     assert new_intent == "<ph_0> and <ph_1>"
-    assert uncanonicalize(new_mr, ph2mr) == mr
+    assert uncanonicalize(new_mr, ph2mr) == original_mr
     assert len(ph2mr) == 2
 
 def test_canonicalize_variable_store():
     intent = "`var1` and 'var2'"
     snippet = "var1 = var2"
     mr = ast_to_mr(ast.parse(snippet))
+    original_mr = deepcopy(mr)
     new_intent, new_mr, ph2mr = canonicalize(intent, mr)
     assert new_intent == "<ph_0> and <ph_1>"
-    assert uncanonicalize(new_mr, ph2mr) == mr
+    assert uncanonicalize(new_mr, ph2mr) == original_mr
     assert len(ph2mr) == 2
 
 def test_canonicalize_weird_string():
     intent = "Hello 'ef-sv-3=:'"
     snippet = "print('ef-sv-3=:')"
     mr = ast_to_mr(ast.parse(snippet))
+    original_mr = deepcopy(mr)
     new_intent, new_mr, ph2mr = canonicalize(intent, mr)
     assert new_intent == "Hello <ph_0>"
-    assert uncanonicalize(new_mr, ph2mr) == mr
+    assert uncanonicalize(new_mr, ph2mr) == original_mr
     assert len(ph2mr) == 1
 
 def test_canonicalize_duplicate_intent():
     intent = "create `result` from `list1` if first element of `list1` is in `list2`"
     snippet = "result = [x for x in list1 if x[0] in list2]"
     mr = ast_to_mr(ast.parse(snippet))
+    original_mr = deepcopy(mr)
     new_intent, new_mr, ph2mr = canonicalize(intent, mr)
     assert new_intent == "create <ph_0> from <ph_1> if first element of <ph_1> is in <ph_2>"
-    assert uncanonicalize(new_mr, ph2mr) == mr
+    assert uncanonicalize(new_mr, ph2mr) == original_mr
     assert len(ph2mr) == 3
