bda37d37c0295fede7c3752201be7aceeb85cffa
YizhePKU <yizhe@pku.edu.cn>
2021-09-23 01:32:37 +0800

    Better Tensorboard default

--- src/utils/tensorboard.py
+++ src/utils/tensorboard.py
@@ -1,11 +1,10 @@
 import torch
 import torch.utils.tensorboard
 
-writer = torch.utils.tensorboard.SummaryWriter()
+writer = torch.utils.tensorboard.SummaryWriter(max_queue=1)
 
 profiler = torch.profiler.profile(
     schedule=torch.profiler.schedule(wait=2, warmup=2, active=6, repeat=1),
     on_trace_ready=torch.profiler.tensorboard_trace_handler("runs"),
     with_stack=True,
-    record_shapes=True,
 )
