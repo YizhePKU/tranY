e9d00e027470c5c3dabac647682ef201daad44d1
YizhePKU <yizhe@pku.edu.cn>
2021-09-23 16:46:07 +0800

    Use clearer log_dir for tensorboard

--- src/utils/tensorboard.py
+++ src/utils/tensorboard.py
@@ -1,10 +1,17 @@
+from pathlib import Path
+from datetime import datetime
 import torch
 import torch.utils.tensorboard
 
-writer = torch.utils.tensorboard.SummaryWriter(max_queue=1)
+log_dir = Path("runs") / datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+writer = torch.utils.tensorboard.SummaryWriter(
+    log_dir=log_dir,
+    max_queue=1,
+)
 
 profiler = torch.profiler.profile(
     schedule=torch.profiler.schedule(wait=2, warmup=2, active=6, repeat=1),
-    on_trace_ready=torch.profiler.tensorboard_trace_handler("runs"),
+    on_trace_ready=torch.profiler.tensorboard_trace_handler(log_dir),
     with_stack=True,
 )
