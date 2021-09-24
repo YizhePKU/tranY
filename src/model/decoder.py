3f77e4476f6e29538104017883531c5c671a8434
YizhePKU <yizhe@pku.edu.cn>
2021-09-24 13:36:30 +0800

    Simplify decoder

--- src/model/decoder.py
+++ src/model/decoder.py
@@ -15,24 +15,18 @@ class DecoderLSTM(nn.Module):
         self.lstm = nn.LSTM(embedding_dim, hidden_size, device=device)
         self.out = nn.Linear(hidden_size, vocab_size, device=device)
 
-    def forward(self, input, hidden):
+    def forward(self, input_actions, hidden):
         """Feed actions to the decoder.
 
         Args:
-            input (seq_length x batch_size): ids of the input actions.
+            input_actions (seq_length x batch_size): ids of the input actions.
             hidden: previous hidden state. defaults to zeros if not provided or None.
 
         Returns:
             logits (batch_size x vocab_size): prediction for the next action.
             hidden: new hidden state.
         """
-        # embedded: (seq_length x batch_size x embedding_dim)
-        embedded = self.embedding(input)
-        # embedded_relu: (seq_length x batch_size x embedding_dim)
-        embedded_relu = F.relu(embedded)
-        embedded_relu_dropped = self.dropout(embedded_relu)
-        # output: (seq_length x batch_size x hidden_size)
-        output, hidden = self.lstm(embedded_relu_dropped, hidden)
-        # logits: (batch_size x vocab_size)
+        action_embeddings = self.embedding(input_actions)
+        output, hidden = self.lstm(action_embeddings, hidden)
         logits = self.out(output[-1])
         return logits, hidden
