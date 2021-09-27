99f9f3c64fc15b5b157b8909bb3b516f346b738a
YizhePKU <yizhe@pku.edu.cn>
2021-09-27 20:29:14 +0800

    Implement new model based on lstm_copy.py

--- src/cfg.py
+++ src/cfg.py
@@ -17,15 +17,19 @@ seed = 47
 EncoderLSTM = {
     "embedding_dim": 64,
     "hidden_size": 128,
-    "dropout_p": 0.0,
+    "nlayers": 1,
+    "dropout_p": 0.3,
+    "bidirectional": True,
 }
 DecoderLSTM = {
     "embedding_dim": 64,
     "hidden_size": 256,
+    "nlayers": 1,
+    "dropout_p": 0.3,
+    "context_size": 256, # 2 * encoder.hidden_size
 }
 
 # training
-learning_rate = 1e-2
+learning_rate = 1e-3
+batch_size = 32
 max_recipe_len = 100
-batch_size = 16
-teacher_forcing_p = 1
--- src/main.py
+++ src/main.py
@@ -12,74 +12,59 @@ import torch.optim as optim
 import cfg
 from asdl.parser import parse as parse_asdl
 from data.conala import ConalaDataset
-from model.decoder import DecoderLSTM
-from model.encoder import EncoderLSTM
-from model.seq2seq import Seq2Seq
+from model.seq2seq_v2 import DecoderLSTM, EncoderDecoder, EncoderLSTM
 from utils.tensorboard import profiler, writer
 
 
-def load(ds):
-    """Wrap a Dataset in a DataLoader.
-
-    Variable-length inputs are packed into PackedSequence, which can be fed
-    to the seq2seq model directly.
-    """
+def load(ds, shuffle=False):
+    """Wrap a Dataset in a DataLoader."""
 
     def collate_fn(data):
+        input = torch.nn.utils.rnn.pad_sequence([input for input, _ in data])
+        input_lengths = [len(input) for input, _ in data]
+        label = torch.nn.utils.rnn.pad_sequence([label for _, label in data])
+        label_lengths = [len(label) for _, label in data]
         return (
-            torch.nn.utils.rnn.pack_sequence(
-                [input for input, _ in data], enforce_sorted=False
-            ).to(cfg.device),
-            torch.nn.utils.rnn.pack_sequence(
-                [label for _, label in data], enforce_sorted=False
-            ).to(cfg.device),
+            input.to(cfg.device),
+            label.to(cfg.device),
+            torch.tensor(input_lengths, device=cfg.device),
+            torch.tensor(label_lengths, device=cfg.device),
         )
 
     return torch.utils.data.DataLoader(
         ds,
         batch_size=cfg.batch_size,
-        shuffle=True,
+        shuffle=shuffle,
         collate_fn=collate_fn,
     )
 
 
 def calculate_loss(logits, label):
-    assert type(label) == torch.nn.utils.rnn.PackedSequence
-    label, _ = torch.nn.utils.rnn.pad_packed_sequence(label)
     return F.cross_entropy(
         logits.reshape(-1, logits.shape[-1]), label.flatten(), reduction="sum"
     )
 
 
 def calculate_errors(logits, label):
-    assert type(label) == torch.nn.utils.rnn.PackedSequence
-    label, _ = torch.nn.utils.rnn.pad_packed_sequence(label)
     return torch.sum(torch.argmax(logits, dim=2) != label)
 
 
 def train_epoch(model, ds, optimizer):
     model.train()
-    for batch in load(ds):
+    for input, label, input_lengths, label_lengths in load(ds):
         optimizer.zero_grad()
-        input, label = batch
-        logits = model(
-            input,
-            label,
-            max_recipe_len=cfg.max_recipe_len,
-            teacher_forcing_p=cfg.teacher_forcing_p,
-        )
+        logits = model(input, label, input_lengths, label_lengths)
         loss = calculate_loss(logits, label)
         loss.backward()
         optimizer.step()
 
 
-def evaluate(model, ds, teacher_forcing_p=0):
+def evaluate(model, ds):
     """Evaluate model on a dataset.
 
     Args:
         model (seq2seq.model.Seq2Seq): seq2seq model to evaluate.
         ds (Dataset): dataset to evaluate on.
-        teacher_forcing_p (float): how often to use teacher forcing.
 
     Returns:
         loss (float): average loss per sample.
@@ -89,12 +74,12 @@ def evaluate(model, ds, teacher_forcing_p=0):
     with torch.no_grad():
         loss = 0
         errors = 0
-        for input, label in load(ds):
+        for input, label, input_length, label_length in load(ds):
             logits = model(
                 input,
                 label,
-                max_recipe_len=cfg.max_recipe_len,
-                teacher_forcing_p=teacher_forcing_p,
+                input_length,
+                label_length,
             )
             loss += calculate_loss(logits, label).item()
             errors += calculate_errors(logits, label).item()
@@ -111,8 +96,6 @@ def main():
     grammar = parse_asdl("src/asdl/Python.asdl")
 
     # load CoNaLa intent-snippet pairs and map them to tensors
-    # FIXME: train_ds and dev_ds are using different word and action mappings.
-    # This causes dev performance to become completely nonsense.
     dataset_cache = cfg.model_dir / "dataset_cache.pt"
     if dataset_cache.exists():
         train_ds, dev_ds = torch.load(dataset_cache)
@@ -122,22 +105,27 @@ def main():
             "data/conala-train.json", grammar=grammar, special_tokens=special_tokens
         )
         dev_ds = ConalaDataset(
-            "data/conala-dev.json", grammar=grammar, special_tokens=special_tokens
+            "data/conala-dev.json",
+            grammar=grammar,
+            special_tokens=special_tokens,
+            action_vocab=train_ds.action_vocab,
+            intent_vocab=train_ds.intent_vocab,
+            shuffle=False,
         )
         torch.save((train_ds, dev_ds), dataset_cache)
 
     # initialize the model and optimizer
     encoder = EncoderLSTM(
-        vocab_size=train_ds.word_vocab_size,
+        vocab_size=train_ds.intent_vocab_size,
         device=cfg.device,
         **cfg.EncoderLSTM,
     )
     decoder = DecoderLSTM(
-        vocab_size=train_ds.action_vocab_size,
+        action_vocab_size=train_ds.action_vocab_size,
         device=cfg.device,
         **cfg.DecoderLSTM,
     )
-    model = Seq2Seq(encoder, decoder, special_tokens=special_tokens, device=cfg.device)
+    model = EncoderDecoder(encoder, decoder, device=cfg.device)
     optimizer = optim.Adam(model.parameters(), lr=cfg.learning_rate)
 
     # load checkpoints
@@ -159,9 +147,6 @@ def main():
         train_loss, train_errors = evaluate(model, train_ds)
         writer.add_scalar("Train/loss", train_loss, epoch)
         writer.add_scalar("Train/errors", train_errors, epoch)
-        train_tf_loss, train_tf_errors = evaluate(model, train_ds, teacher_forcing_p=1)
-        writer.add_scalar("Train/loss(teacher_forcing_p=1)", train_tf_loss, epoch)
-        writer.add_scalar("Train/errors(teacher_forcing_p=1)", train_tf_errors, epoch)
         dev_loss, dev_errors = evaluate(model, dev_ds)
         writer.add_scalar("Dev/loss", dev_loss, epoch)
         writer.add_scalar("Dev/errors", dev_errors, epoch)
